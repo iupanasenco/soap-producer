588a72580a9128fc65d6ce1f5c97aa3c249e3b94
ipanasenco <panasenco.iunona@gmail.com>
2020-12-14 15:51:33 +0200

    Initial commit

---
+++ src/test/java/com/trip/soapproducer/SoapProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.soapproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
